471c8e20fc662198e52a871a2272a5f82319ae45
Arthals <zhuozhiyongde@126.com>
2025-12-18 01:42:12 +0800

    ✨ feat: Deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: 'BML',
+            script: 'PORT=24704 bun run start',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+        },
+    ]
+}
+
